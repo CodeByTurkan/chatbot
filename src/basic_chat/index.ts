5f71a0d7484fae476cd0ec3fb621bbfb6b7e15b4
Turkan Isayeva <turkantech@gmail.com>
2026-03-09 17:27:06 +0400

    prevent resending whole messages to the chat

--- src/basic_chat/index.ts
+++ src/basic_chat/index.ts
@@ -1,11 +1,14 @@
 import OpenAI from "openai";
+import { encoding_for_model } from "tiktoken";
 
 const client = new OpenAI();
-
+const encoder = encoding_for_model('gpt-4.1-mini')
+  
 type RoleProps = {
   role: "user" | "assistant" | "system";
   content: string;
 }
+const MAX_TOKENS = 700;
 
 const context: RoleProps [] = [
   {
@@ -16,7 +19,7 @@ const context: RoleProps [] = [
 
 async function createChatCompletion() {
   const response = await client.responses.create({
-    model: "gpt-5.4",
+    model: "gpt-4.1-mini",
     input: context,
   });
   const responseMessage = response.output_text;
@@ -24,6 +27,9 @@ async function createChatCompletion() {
     role: "assistant",
     content: responseMessage,
   });
+  if (response.usage && response.usage.total_tokens > MAX_TOKENS) {
+    await deleteChatMessages();
+  }
   console.log(`Assistant: ${response.output_text}`);
 }
 
@@ -35,3 +41,38 @@ process.stdin.addListener("data", async function name(input) {
   });
   createChatCompletion();
 });
+
+async function deleteChatMessages() {
+  let contextLength = await getContextLength();
+  while (contextLength > MAX_TOKENS) {
+    let removed = false;
+    for (let index = 1; index < context.length; index++) {
+      const input = context[index];
+      if (input.role !== "system") {
+        context.splice(index, 1);
+        contextLength = await getContextLength();
+        console.log("New context length:" + contextLength);
+        removed = true;
+        break;
+      }
+    }
+    if (!removed) break; 
+  }
+}
+
+async function getContextLength() {
+  let length = 0;
+  context.forEach((input) => {
+    if (typeof input.content == "string") {
+      length += encoder.encode(input.content).length
+    } else if (Array.isArray(input.content)) {
+      input.content.forEach((inputContent) => {
+        if (inputContent.type =="text") {
+                length += encoder.encode(inputContent.content).length;
+
+        }
+      });
+    }
+  })
+  return length
+}
