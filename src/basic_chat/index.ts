d78533ff9e9b51885cfe7acbeb858b85fb03ab83
Turkan Isayeva <turkantech@gmail.com>
2026-03-09 15:19:13 +0400

    added contect window

--- src/basic_chat/index.ts
+++ src/basic_chat/index.ts
@@ -2,22 +2,36 @@ import OpenAI from "openai";
 
 const client = new OpenAI();
 
-process.stdin.addListener("data", async function name(input) {
-  const userInput = input.toString().trim();
+type RoleProps = {
+  role: "user" | "assistant" | "system";
+  content: string;
+}
+
+const context: RoleProps [] = [
+  {
+    role: "system",
+    content: "You are a crazy chatbot assistant",
+  },
+];  
+
+async function createChatCompletion() {
   const response = await client.responses.create({
     model: "gpt-5.4",
-    input: [
-      {
-        role: "system",
-        content: "You are a crazt chatbot assitant",
-      },
-      {
-          role: "user",
-          content: userInput
-      },
-    ],
+    input: context,
   });
-    console.log(response.output_text);
+  const responseMessage = response.output_text;
+  context.push({
+    role: "assistant",
+    content: responseMessage,
+  });
+  console.log(`Assistant: ${response.output_text}`);
+}
 
+process.stdin.addListener("data", async function name(input) {
+  const userInput = input.toString().trim();
+  context.push({
+    role: "user",
+    content: userInput,
+  });
+  createChatCompletion();
 });
-
